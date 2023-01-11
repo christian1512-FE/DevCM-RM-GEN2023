6f7d855a3709c21e945efa6797c2d1b9b2068e7b
Christian Mendez <christianmendez@Christians-MBP.attlocal.net>
2023-01-11 14:50:48 -0600

    module 9 questions added

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,88 @@
 // TODO: Include packages needed for this application
+//TO USE INQUIRE YOU HAVE TO IMPORTER IT.
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        //TITLE OF PROJECT
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your project:'
+    },
+
+    {
+        //INSTALLATION INSTRUCTIONS
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:'
+    },
+
+    {
+        //USAGE INFORMATION
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:'
+    },
+
+    {
+        //CONTRIBUTION GUIDELINES
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter Contribution guidelines:'
+    },
+
+        //TEST INSTRUCTIONS
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter test instructions:'
+    },
+
+    {
+        // LICENSE
+        type: 'input',
+        name: 'license',
+        message: 'Choose a license your application from the list options:',
+        //ADD THE LIST
+    },
+
+    {
+        //GITHUB USERNAME
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username:',
+    },
+
+    {
+        //EMAIL
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    }
+
+];
+
+
+//METHOD PROMPT ACCEPTS AN ARGUMENT (QUESTIONS)
+inquirer
+    .prompt(questions)
+    .then((response) => {
+        console.log(response);
+    });
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
-init();
+init(questions);
+
+
+//TO USE INQUIRE YOU HAVE TO IMPORTER IT.
+// const inquirer = require('inquirer');
